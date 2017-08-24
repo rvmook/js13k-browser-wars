7c069b87f95bda67d3a918609b121beddee990db
Rick van Mook <rick@rvmook.com>
2017-08-24 00:01:10 -0400

    Added ship drawing on canvas

--- public/client.js
+++ public/client.js
@@ -1,13 +1,38 @@
+const MAX_DISTANCE = 100;
+
 var socket = io(),
+	playerId,
+	width,
+	height,
+	halfWidth,
+	halfHeight,
+	newRadians,
+	newSpeed,
+	currentRadians,
+	currentSpeed,
 	win = window,
 	doc = document,
+	ctx = c.getContext('2d'),
 	currentKey = '',
 	isMobile = win.matchMedia('(max-width:440px)').matches,
 	isChrome = !!win.chrome && !!win.chrome.webstore,
 	isFirefox = typeof InstallTrigger !== 'undefined';
 
 socket.on(CONNECTION_GRANTED,onConnectionGranted);
-console.log('isMobile', isMobile);
+
+// fix bug in safari: http://qfox.nl/weblog/218
+document.body.clientWidth;
+// auto resize (original) canvas. call `onresize(w,h) to limit the size of the canvas
+win.onresize = function(){
+
+	c.style.width = (c.width = width = innerWidth) + 'px';
+	c.style.height = (c.height = height = innerHeight) + 'px';
+	halfWidth = width/2;
+	halfHeight = height/2;
+};
+
+win.onresize();
+redraw();
 
 if(!isMobile) {
 
@@ -25,10 +50,6 @@ if(!isMobile) {
 	connectTo(key);
 }
 
-doc.querySelectorAll('li').forEach((el)=>{
-
-	el.addEventListener('click', ()=>{connectTo(el.innerHTML)});
-});
 
 
 function connectTo(key) {
@@ -41,11 +62,76 @@ function connectTo(key) {
 	socket.emit(REQUEST_CONNECTION, currentKey, isMobile);
 }
 
-function onConnectionGranted(playerId) {
+function onConnectionGranted(newPlayerId) {
+
+	playerId = newPlayerId;
 	console.log('playerId', playerId);
 }
 
 function onUpdate(updated) {
 
 	console.log('isMobile', isMobile, 'updated', updated);
+win.addEventListener('touchend',()=>{newSpeed = 0});
+win.addEventListener('touchmove', win.onmousemove = (e)=>{
+
+	if(e.touches) {
+
+		e = e.touches[0];
+	}
+
+	let x = e.clientX,
+		y = e.clientY;
+
+	newRadians = Math.atan2(halfHeight-y, halfWidth-x);
+	newSpeed = Math.max(0, Math.min(1, distance(x, y, halfWidth, halfHeight) / MAX_DISTANCE));
+});
+
+function distance(x1, y1, x2, y2) {
+
+	var a = x1 - x2;
+	var b = y1 - y2;
+
+	return Math.sqrt( a*a + b*b );
+}
+
+setInterval(()=>{
+	if(newRadians !== currentRadians || newSpeed !== currentSpeed) {
+
+		currentRadians = newRadians;
+		currentSpeed = newSpeed;
+
+		socket.emit(PLAYER_UPDATE, playerId, currentRadians, currentSpeed);
+	}
+}, 1000/30);
+
+function redraw() {
+
+	ctx.clearRect(0, 0, width, height);
+
+	drawShip(0, 0, currentRadians, currentSpeed);
+
+
+	requestAnimationFrame(redraw);
+}
+
+function drawShip(x = 0, y = 0, radians = 0, speed = 0) {
+
+	ctx.save();
+
+	ctx.beginPath();
+
+	ctx.translate(halfWidth + x, halfHeight + y);
+
+	ctx.rotate(radians);
+
+	ctx.moveTo(-20, 0);
+	ctx.lineTo(10, -10);
+	ctx.lineTo(0, 0);
+	ctx.lineTo(10, 10);
+	ctx.closePath();
+	ctx.fillStyle = 'yellow';
+	ctx.fill();
+
+	ctx.restore();
+
 }
